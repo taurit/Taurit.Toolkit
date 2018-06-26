50aba7924231c4fc75e4553be3da23092f9f611b
Taurit <git@taurit.pl>
2018-06-26 21:08:07 +0200

    Add CountdownToSleep util

---
+++ Taurit.Toolkit.ContdownToSleep/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Taurit.Toolkit.ContdownToSleep
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Taurit.Toolkit.ContdownToSleep/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+using Taurit.Toolkit.ContdownToSleep.Annotations;
+
+namespace Taurit.Toolkit.ContdownToSleep
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private static readonly TimeSpan TimerInterval = TimeSpan.FromSeconds(1);
+        private readonly DispatcherTimer _countdownTimer;
+
+        public MainWindow()
+        {
+            Time = TimeSpan.FromMinutes(10);
+
+            InitializeComponent();
+            SetLocationToRightBottomCorner();
+
+            _countdownTimer = new DispatcherTimer(DispatcherPriority.Normal, Application.Current.Dispatcher);
+            _countdownTimer.Tick += CountdownTimerOnTick;
+            _countdownTimer.Interval = new TimeSpan(0, 0, 1);
+            _countdownTimer.Start();
+        }
+
+        public TimeSpan Time { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetLocationToRightBottomCorner()
+        {
+            const Int32 margin = 20;
+            Rect desktopWorkingArea = SystemParameters.WorkArea;
+            Left = desktopWorkingArea.Right - Width - margin;
+            Top = desktopWorkingArea.Bottom - Height - margin;
+        }
+
+        private void CountdownTimerOnTick(Object sender, EventArgs e)
+        {
+            if (Time.TotalMilliseconds <= 0)
+            {
+                _countdownTimer.Stop();
+
+                // Suspend (go to sleep mode)
+                SetSuspendState(false, true, true);
+
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                Time = Time.Subtract(TimerInterval);
+                OnPropertyChanged(nameof(Time));
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern Boolean SetSuspendState(Boolean hiberate, Boolean forceCritical, Boolean disableWakeEvent);
+    }
+}
