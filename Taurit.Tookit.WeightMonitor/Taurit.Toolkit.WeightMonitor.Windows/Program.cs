33618baa5a8fa63b99fd08c020bf7e3c45b82cc2
Taurit <git@taurit.pl>
2018-07-13 19:16:13 +0200

    Add proof of concept that obtains weight from Google Fit via REST API

--- Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.Windows/Program.cs
+++ Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.Windows/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Fitness.v1;
+using Google.Apis.Fitness.v1.Data;
+using Google.Apis.Services;
+using NodaTime;
 
 namespace Taurit.Toolkit.WeightMonitor.Windows
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(String[] args)
         {
+            UserCredential credential;
+            using (var stream = new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] {FitnessService.Scope.FitnessBodyWrite},
+                    "user", CancellationToken.None);
+            }
+
+            var service = new FitnessService(new BaseClientService.Initializer
+            {
+                ApplicationName = Assembly.GetExecutingAssembly().GetName().Name,
+                HttpClientInitializer = credential
+            });
+
+            Instant now = SystemClock.Instance.GetCurrentInstant();
+            Int64 from = now.Minus(Duration.FromDays(30)).ToUnixTimeMilliseconds() * 1_000_000;
+            Int64 to = now.ToUnixTimeMilliseconds() * 1_000_000;
+
+            Dataset weightPoints = service
+                .Users
+                .DataSources
+                .Datasets
+                .Get("me", "derived:com.google.weight:com.google.android.gms:merge_weight", $"{from}-{to}")
+                .Execute();
         }
     }
 }
