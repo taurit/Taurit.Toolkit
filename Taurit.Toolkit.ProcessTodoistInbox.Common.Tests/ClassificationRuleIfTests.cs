2316b4830b5082eafd989578f7918d464959f80a
Taurit <git@taurit.pl>
2018-08-05 10:03:30 +0200

    WeightMonitor: update project dependencies

--- Taurit.Toolkit.ProcessTodoistInbox.Common.Tests/ClassificationRuleIfTests.cs
+++ Taurit.Toolkit.ProcessTodoistInbox.Common.Tests/ClassificationRuleIfTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Taurit.Toolkit.ProcesTodoistInbox.Common.Models.Classification;
 using Taurit.Toolkit.TodoistInboxHelper.ApiModels;
+#pragma warning disable 618 // I'm not using constructor for ClassificationRuleIf to make tests brief
 
 namespace Taurit.Toolkit.ProcessTodoistInbox.Common.Tests
 {
