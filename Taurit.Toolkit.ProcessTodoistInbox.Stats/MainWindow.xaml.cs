6a5d70a227bb2df1fddf7a501c27d5495d566b55
Taurit <git@taurit.pl>
2018-11-11 14:23:01 +0100

    Add chart for number of tasks

--- Taurit.Toolkit.ProcessTodoistInbox.Stats/MainWindow.xaml.cs
+++ Taurit.Toolkit.ProcessTodoistInbox.Stats/MainWindow.xaml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using JetBrains.Annotations;
 using LiveCharts;
 using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 using Taurit.Toolkit.ProcessTodoistInbox.Stats.Models;
 using Taurit.Toolkit.ProcessTodoistInbox.Stats.Services;
-using Taurit.Toolkit.TodoistInboxHelper;
+using Taurit.Toolkit.TodoistInboxHelper.ApiModels;
 
 namespace Taurit.Toolkit.ProcessTodoistInbox.Stats
 {
@@ -17,8 +19,8 @@ namespace Taurit.Toolkit.ProcessTodoistInbox.Stats
     /// </summary>
     public partial class MainWindow : Window
     {
-        private StatsAppSettings _settings;
-        private SnapshotReader _snapshotReader;
+        private readonly StatsAppSettings _settings;
+        private readonly SnapshotReader _snapshotReader;
 
         public MainWindow()
         {
@@ -27,28 +29,77 @@ namespace Taurit.Toolkit.ProcessTodoistInbox.Stats
             _snapshotReader = new SnapshotReader();
         }
 
-        public Func<Double, String> XFormatter { get; } = value =>
-        {
-            return new DateTime((Int64) value).ToString("yyyy-MM-dd");
-        };
+        public Func<Double, String> XFormatter { get; } = value => new DateTime((Int64) value).ToString("yyyy-MM-dd");
+
+        public SeriesCollection NumberOfTasks { get; set; } = new SeriesCollection();
 
         [NotNull]
-        public ChartValues<DateTimePoint> YData { get; } = new ChartValues<DateTimePoint>();
+        public ChartValues<DateTimePoint> NumberOfLowPriorityTasks { get; } = new ChartValues<DateTimePoint>();
 
         private void RadioButtonSetupChanged([CanBeNull] Object sender, [CanBeNull] RoutedEventArgs e)
         {
             if (!IsInitialized) return;
-            YData.Clear();
+            NumberOfLowPriorityTasks.Clear();
 
             TimeSpan selectedTimePeriod = GetSelectedTimePeriod();
             List<SnapshotOnTimeline> snapshotsInSelectedTimePeriod =
                 _snapshotReader.Read(_settings.SnapshotsRootFolderPath, DateTime.UtcNow, selectedTimePeriod);
 
-            foreach (var snapshot in snapshotsInSelectedTimePeriod)
+            var undefinedPriorityTasks = new List<DateTimePoint>();
+            var lowPriorityTasks = new List<DateTimePoint>();
+            var mediumPriorityTasks = new List<DateTimePoint>();
+            var highPriorityTasks = new List<DateTimePoint>();
+
+            foreach (SnapshotOnTimeline snapshot in snapshotsInSelectedTimePeriod)
             {
-                YData.Add(new DateTimePoint(snapshot.Time, snapshot.AllTasks.Count));
+                List<TodoTask> tasksWithNoDueDate = snapshot.AllTasks.Where(x => !x.HasDate).ToList();
+                Int32 numUndefinedPriorityTasks = tasksWithNoDueDate.Count(x => x.priority == 1);
+                Int32 numLowPriorityTasks = tasksWithNoDueDate.Count(x => x.priority == 2);
+                Int32 numMediumPriorityTasks = tasksWithNoDueDate.Count(x => x.priority == 3);
+                Int32 numHighPriorityTasks = tasksWithNoDueDate.Count(x => x.priority == 4);
+
+                undefinedPriorityTasks.Add(new DateTimePoint(snapshot.Time, numUndefinedPriorityTasks));
+                lowPriorityTasks.Add(new DateTimePoint(snapshot.Time, numLowPriorityTasks));
+                mediumPriorityTasks.Add(new DateTimePoint(snapshot.Time, numMediumPriorityTasks));
+                highPriorityTasks.Add(new DateTimePoint(snapshot.Time, numHighPriorityTasks));
             }
 
+            var series = new[]
+            {
+                new StackedAreaSeries
+                {
+                    Title = "High priority",
+                    Values = new ChartValues<DateTimePoint>(highPriorityTasks),
+                    LineSmoothness = 0,
+                    Fill = new SolidColorBrush(Color.FromRgb(224, 87, 85))
+                    //Fill = new SolidColorBrush(Color.FromRgb(252, 237, 237)),
+                },
+                new StackedAreaSeries
+                {
+                    Title = "Medium priority",
+                    Values = new ChartValues<DateTimePoint>(mediumPriorityTasks),
+                    LineSmoothness = 0,
+                    Fill = new SolidColorBrush(Color.FromRgb(255, 198, 149))
+                    //Fill = new SolidColorBrush(Color.FromRgb(255, 246, 238)),
+                },
+                new StackedAreaSeries
+                {
+                    Title = "Low priority",
+                    Values = new ChartValues<DateTimePoint>(lowPriorityTasks),
+                    LineSmoothness = 0,
+                    Fill = new SolidColorBrush(Color.FromRgb(255, 232, 174))
+                    //Fill = new SolidColorBrush(Color.FromRgb(255, 251, 241)),
+                },
+                new StackedAreaSeries
+                {
+                    Title = "Undefined priority",
+                    Values = new ChartValues<DateTimePoint>(undefinedPriorityTasks),
+                    LineSmoothness = 0,
+                    Fill = new SolidColorBrush(Color.FromRgb(235, 235, 235))
+                    //Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255)),
+                }
+            };
+            NumberOfTasks.AddRange(series);
         }
 
         private TimeSpan GetSelectedTimePeriod()
