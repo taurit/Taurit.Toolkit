75d3a8384d215a59084e98979fc600d6e97da998
Taurit <git@taurit.pl>
2018-11-11 12:38:15 +0100

    Add basic draft of the main window

--- Taurit.Toolkit.ProcessTodoistInbox.Stats/MainWindow.xaml.cs
+++ Taurit.Toolkit.ProcessTodoistInbox.Stats/MainWindow.xaml.cs
@@ -1,28 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using JetBrains.Annotations;
+using LiveCharts;
+using LiveCharts.Defaults;
 
 namespace Taurit.Toolkit.ProcessTodoistInbox.Stats
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private StatsAppSettings _settings;
+
         public MainWindow()
         {
             InitializeComponent();
+            _settings = new StatsAppSettings();
+        }
+
+        public Func<Double, String> XFormatter { get; } = value =>
+        {
+            return new DateTime((Int64) value).ToString("yyyy-MM-dd");
+        };
+
+        [NotNull]
+        public ChartValues<DateTimePoint> YData { get; } = new ChartValues<DateTimePoint>();
+
+        private void RadioButtonSetupChanged([CanBeNull] Object sender, [CanBeNull] RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+            YData.Clear();
+
+            TimeSpan selectedTimePeriod = GetSelectedTimePeriod();
+
+
+            YData.Add(new DateTimePoint(DateTime.Now, 666));
+            YData.Add(new DateTimePoint(DateTime.Now.AddDays(1), 666));
+            YData.Add(new DateTimePoint(DateTime.Now.AddDays(3), 766));
+        }
+
+        private TimeSpan GetSelectedTimePeriod()
+        {
+            RadioButton[] timePeriodCheckboxes =
+            {
+                Time_AllTime,
+                Time_LastMonth,
+                Time_LastWeek,
+                Time_LastYear
+            };
+            RadioButton selectedTimeCheckbox = timePeriodCheckboxes.Single(x => x.IsChecked == true);
+            Int32 timeInDays = Convert.ToInt32((String) selectedTimeCheckbox.Tag);
+            return TimeSpan.FromDays(timeInDays);
+        }
+
+
+        private void MainWindow_OnContentRendered(Object sender, EventArgs e)
+        {
+            RadioButtonSetupChanged(null, null);
         }
     }
 }
---
+++ Taurit.Toolkit.ProcessTodoistInbox.Stats/StatsAppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Taurit.Toolkit.ProcessTodoistInbox.Stats
+{
+    // todo move to some external configuration file
+    internal class StatsAppSettings
+    {
+        public String SnapshotsRootFolderPath =>
+            @"\\192.168.0.122\c$\Data\Users\DefaultAccount\AppData\Local\Packages\Taurit.Toolkit.ProcessTodoistInboxBackground-uwp_yyw9gtkk70966\LocalState\BacklogSnapshots\";
+    }
+}
