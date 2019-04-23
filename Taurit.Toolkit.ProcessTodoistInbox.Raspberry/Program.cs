766b116d40e6620720e9796e219b7473ac5ceccb
Taurit <git@taurit.pl>
2019-04-23 17:33:46 +0200

    Add new project to allow move inbox processing from Win10 IOT to Raspbian

---
+++ Taurit.Toolkit.ProcessTodoistInbox.Raspberry/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Taurit.Toolkit.ProcessTodoistInbox.Raspberry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Yo dawg");
+            Console.ReadLine();
+        }
+    }
+}
