c2aa6eaded0ec769d81e5dac394ba7714950a88e
Taurit <git@taurit.pl>
2019-11-03 07:13:36 +0100

    Convert 'Taurit.Toolkit.ProcessRecognizedInboxFiles.Tests' to netcore3

---
+++ Taurit.Toolkit.FileProcessors.Tests/IsoDateFileNameProviderTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Taurit.Toolkit.FileProcessors.NameProcessors.NameFormatProviders;
+using Xunit;
+
+namespace Taurit.Toolkit.FixDateFormatInFilenames.Tests
+{
+    public class IsoDateFileNameProviderTests
+    {
+        [Fact]
+        public void OneDigitDay_ShouldBePaddedWithZeroInOutput()
+        {
+            // Arrange
+            var sut = new IsoDateFileNameFormatProvider();
+
+            // Act
+            String isoName1 = sut.FormatFileName("2000", "12", "01", "Test");
+            String isoName2 = sut.FormatFileName("2000", "12", "1", "Test");
+
+            // Assert
+            Assert.Equal("2000-12-01 Test", isoName1);
+            Assert.Equal("2000-12-01 Test", isoName2);
+        }
+
+        [Fact]
+        public void OneDigitMonth_ShouldBePaddedWithZeroInOutput()
+        {
+            // Arrange
+            var sut = new IsoDateFileNameFormatProvider();
+
+            // Act
+            String isoName1 = sut.FormatFileName("2000", "01", "11", "Test");
+            String isoName2 = sut.FormatFileName("2000", "2", "11", "Test");
+
+            // Assert
+            Assert.Equal("2000-01-11 Test", isoName1);
+            Assert.Equal("2000-02-11 Test", isoName2);
+        }
+
+        [Fact]
+        public void TwoDigitYear_ShouldBeAssumedToBelongTo21thCentury()
+        {
+            // Arrange
+            var sut = new IsoDateFileNameFormatProvider();
+
+            // Act
+            String isoName1 = sut.FormatFileName("19", "01", "11", "Test");
+
+            // Assert
+            Assert.Equal("2019-01-11 Test", isoName1);
+        }
+    }
+}
---
+++ Taurit.Toolkit.FileProcessors.Tests/OfficeLensFileNameParserTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using Moq;
+using Taurit.Toolkit.FileProcessors.NameProcessors;
+using Taurit.Toolkit.FileProcessors.NameProcessors.NameFormatProviders;
+using Xunit;
+using Match = System.Text.RegularExpressions.Match;
+
+namespace Taurit.Toolkit.FixDateFormatInFilenames.Tests
+{
+    public class OfficeLensFileNameParserTests
+    {
+        [Fact]
+        public void PostJune2018FilenameFormatIsRecognized()
+        {
+            // Arrange
+            IFileNameFormatProvider formatProviderMock = new Mock<IFileNameFormatProvider>().Object;
+            Debug.Assert(formatProviderMock != null);
+            var sut = new ChangeOfficeLensNameProcessor(formatProviderMock);
+
+            // Act
+            Match match = sut.GetMatch("2099_06_07 11_00 dm pko bp 3.jpg");
+
+            // Assert
+            Assert.NotNull(match);
+            Assert.True(match.Success);
+            Assert.Equal("2099", match.Groups["year"].Value);
+            Assert.Equal("06", match.Groups["month"].Value);
+            Assert.Equal("07", match.Groups["day"].Value);
+            Assert.Equal("dm pko bp 3.jpg", match.Groups["description"].Value);
+        }
+
+        [Fact]
+        public void PreJune2018FilenameFormatIsRecognized()
+        {
+            // Arrange
+            IFileNameFormatProvider formatProviderMock = new Mock<IFileNameFormatProvider>().Object;
+            Debug.Assert(formatProviderMock != null);
+            var sut = new ChangeOfficeLensNameProcessor(formatProviderMock);
+
+            // Act
+            Match match = sut.GetMatch("01.02.2018 11 00 dm pko bp 3.jpg");
+
+            // Assert
+            Assert.NotNull(match);
+            Assert.True(match.Success);
+            Assert.Equal("2018", match.Groups["year"].Value);
+            Assert.Equal("02", match.Groups["month"].Value);
+            Assert.Equal("01", match.Groups["day"].Value);
+            Assert.Equal("dm pko bp 3.jpg", match.Groups["description"].Value);
+        }
+
+        [Fact]
+        public void HpScanToolFilenameFormatIsRecognized()
+        {
+            // Arrange
+            IFileNameFormatProvider formatProviderMock = new Mock<IFileNameFormatProvider>().Object;
+            Debug.Assert(formatProviderMock != null);
+            var sut = new ChangeOfficeLensNameProcessor(formatProviderMock);
+
+            // Act
+            Match match = sut.GetMatch("514316-080118 BUW nauka Content Security Policy v2 features notatki 1.jpg");
+
+            // Assert
+            Assert.NotNull(match);
+            Assert.True(match.Success);
+            Assert.Equal("18", match.Groups["year"].Value);
+            Assert.Equal("01", match.Groups["month"].Value);
+            Assert.Equal("08", match.Groups["day"].Value);
+            Assert.Equal("BUW nauka Content Security Policy v2 features notatki 1.jpg", match.Groups["description"].Value);
+        }
+    }
+}
