27e1510f4666ae1e2bc27dac876fadbf49351eda
Taurit <git@taurit.pl>
2020-02-02 08:26:25 +0100

    Replace source image with source color for simplicity and portability

--- Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.Common/Models/WallpaperConfiguration.cs
+++ Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.Common/Models/WallpaperConfiguration.cs
@@ -4,15 +4,16 @@ namespace Taurit.Toolkit.WeightMonitor.Common.Models
 {
     public class WallpaperConfiguration
     {
-        public String BaseImagePath { get; set; }
-        public String FinalImagePath { get; set; }
-        
+        public Int32 Width { get; set; }
+        public Int32 Height { get; set; }
+        public String BackgroundColor { get; set; }
+
         public Int32? OffsetX { get; set; }
         public Int32? OffsetY { get; set; }
         public Boolean OffsetIsProvided => OffsetX != null && OffsetX >= 0 && OffsetY != null && OffsetY >= 0;
 
         public Boolean GenerateWallpaperWithChart { get; set; }
-
-
+        
+        public String FinalImagePath { get; set; }
     }
 }
--- Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.GUI/Services/WallpaperGenerator.cs
+++ Taurit.Tookit.WeightMonitor/Taurit.Toolkit.WeightMonitor.GUI/Services/WallpaperGenerator.cs
@@ -16,34 +16,31 @@ namespace Taurit.Toolkit.WeightMonitor.GUI.Services
     {
         public void GenerateAugmentedWallpaper(WallpaperConfiguration wallpaperSettings, Grid chartWrapper)
         {
-            var originalWallpaper = new Bitmap(wallpaperSettings.BaseImagePath);
             BitmapFrame chart = VisualToBitmapConverter.Convert(chartWrapper);
 
             // create overlay
-            var wallpaperSize = new Size(1680, 1050);
+            var wallpaperSize = new Size(wallpaperSettings.Width, wallpaperSettings.Height);
 
             var finalImage = new Bitmap(wallpaperSize.Width, wallpaperSize.Height);
-            using (Graphics g = Graphics.FromImage(finalImage))
-            {
-                g.DrawImage(originalWallpaper, new Rectangle(0, 0, 1680, 1050));
-                Bitmap chartBitmap = BitmapFromSource(chart);
+            using Graphics g = Graphics.FromImage(finalImage);
+            using var brush = new SolidBrush(ColorTranslator.FromHtml(wallpaperSettings.BackgroundColor));
+            g.FillRectangle(brush, 0, 0, wallpaperSize.Width, wallpaperSize.Height);
+            Bitmap chartBitmap = BitmapFromSource(chart);
 
-                // center image be default 
-                Int32 offsetX = (wallpaperSize.Width - chartBitmap.Width) / 2;
-                Int32 offsetY = (wallpaperSize.Height - chartBitmap.Height) / 2;
+            // center image be default 
+            Int32 offsetX = (wallpaperSize.Width - chartBitmap.Width) / 2;
+            Int32 offsetY = (wallpaperSize.Height - chartBitmap.Height) / 2;
                 
+            if (wallpaperSettings.OffsetIsProvided)
+            {
+                Debug.Assert(wallpaperSettings.OffsetX != null, "wallpaperSettings.OffsetX != null");
+                Debug.Assert(wallpaperSettings.OffsetY != null, "wallpaperSettings.OffsetY != null");
 
-                if (wallpaperSettings.OffsetIsProvided)
-                {
-                    Debug.Assert(wallpaperSettings.OffsetX != null, "wallpaperSettings.OffsetX != null");
-                    Debug.Assert(wallpaperSettings.OffsetY != null, "wallpaperSettings.OffsetY != null");
-
-                    offsetX = wallpaperSettings.OffsetX.Value;
-                    offsetY = wallpaperSettings.OffsetY.Value;
-                }
-
-                g.DrawImage(chartBitmap, new Rectangle(offsetX, offsetY, chartBitmap.Width, chartBitmap.Height));
+                offsetX = wallpaperSettings.OffsetX.Value;
+                offsetY = wallpaperSettings.OffsetY.Value;
             }
+
+            g.DrawImage(chartBitmap, new Rectangle(offsetX, offsetY, chartBitmap.Width, chartBitmap.Height));
 
             // save resulting file
             ImageCodecInfo jgpEncoder = GetEncoder(ImageFormat.Jpeg);
